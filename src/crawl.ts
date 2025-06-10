a21a2f04104f814efc140e0d545ab1a4a08bf377
xsynch <jdstrickland@gmail.com>
2025-06-09 22:21:04 -0400

    add get url command line arguments. Returns html from url passed

--- src/crawl.ts
+++ src/crawl.ts
@@ -27,3 +27,24 @@ export function getURLsFromHTML(html: string, baseURL: string){
     return linksInPage;
 
 }
+
+export async function getHTML(url: string){
+  
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const headers = response.headers;
+    const pageType = response.headers.get('content-type')
+    if (!pageType || !pageType.startsWith('text/html')){
+        throw new Error(`Content type is not html: ${pageType}`)
+    }
+
+    const body = await response.text();
+    
+    return body;
+  } catch (error) {
+    return error;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import { getHTML } from "./crawl";
+
+async function main(){
+    if (process.argv.length != 3){
+        process.exit('1');
+    }
+    let baseURL = process.argv[2];
+    const results =  await getHTML(baseURL);
+    console.log(results);
+    process.exit(0);
+    
+
+}
+
+main();
